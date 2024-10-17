3ec11e694155b96c48334aaeb5ba1e2c32ae8b25
fundDeveloper22 <xotjrrl@naver.com>
2024-10-17 15:38:10 +0900

    Update mod.rs

--- node/src/prover/mod.rs
+++ node/src/prover/mod.rs
@@ -253,7 +253,7 @@ impl<N: Network, C: ConsensusStorage<N>> Prover<N, C> {
         // Compute the solution.
         let result =
             self.puzzle.prove(epoch_hash, self.address(), rng.gen(), Some(proof_target)).ok().and_then(|solution| {
-                debug!("=== 999 === proof_target : {} solution : {}",proof_target, solution);
+                println!("=== 999 === proof_target : {} solution : {}",proof_target, solution);
                 self.puzzle.get_proof_target(&solution).ok().map(|solution_target| (solution_target, solution))
             });
 
