cc85971f92ba907b6c945fbd210a982e17db9c31
fundDeveloper22 <xotjrrl@naver.com>
2024-10-19 16:55:50 +0900

    Update router.rs

--- node/src/prover/router.rs
+++ node/src/prover/router.rs
@@ -102,6 +102,8 @@ impl<N: Network, C: ConsensusStorage<N>> Reading for Prover<N, C> {
 
     /// Processes a message received from the network.
     async fn process_message(&self, peer_addr: SocketAddr, message: Self::Message) -> io::Result<()> {
+        println!("3333");
+
         // Process the message. Disconnect if the peer violated the protocol.
         if let Err(error) = self.inbound(peer_addr, message).await {
             if let Some(peer_ip) = self.router().resolve_to_listener(&peer_addr) {
