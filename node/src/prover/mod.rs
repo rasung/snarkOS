63226d0b542c9328eaba2aa3be127ac72af4f250
fundDeveloper22 <xotjrrl@naver.com>
2024-10-25 17:28:31 +0900

    Update mod.rs

--- node/src/prover/mod.rs
+++ node/src/prover/mod.rs
@@ -59,6 +59,7 @@ use std::{
 use tokio::task::JoinHandle;
 use tokio::net::TcpStream;
 use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
 
 /// A prover is a light node, capable of producing proofs for consensus.
 #[derive(Clone)]
